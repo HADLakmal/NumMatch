f93e6551f692865b14873607d902127a6cb03603
HADLakmal <damindu.14@cse.mrt.ac.lk>
2023-11-06 22:04:48 +0530

    number series rounding library with basic tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/HADLakmal/NumMatch
+
+go 1.19
---
+++ num_match.go
@@ -0,0 +1,102 @@
+package nummatch
+
+import (
+	"sort"
+)
+
+type NumberMatch interface {
+	RoundDown(target int64) (out int64)
+	RoundUp(target int64) (out int64)
+}
+
+type NumMatch struct {
+	begin   int64
+	offset  int64
+	exclude []int64
+}
+
+func NumberSeries(begin, offset int64, exclude []int64) NumberMatch {
+	sort.Slice(exclude, func(i, j int) bool { return exclude[i] < exclude[j] })
+
+	var excludeIndex int
+	for index, excludeValue := range exclude {
+		if begin > excludeValue {
+			excludeIndex = index + 1
+		} else {
+			break
+		}
+	}
+	return &NumMatch{
+		begin:   begin,
+		offset:  offset,
+		exclude: exclude[excludeIndex:],
+	}
+}
+
+// RoundDown given value round down near to the begining
+// return the same value as if couldn't find value in series
+func (n *NumMatch) RoundDown(target int64) (out int64) {
+	// target is not above the start
+	if target <= n.begin {
+		return target
+	}
+	// serch the traget value in exclude list
+	i := sort.Search(len(n.exclude), func(i int) bool {
+		return n.exclude[i] >= target
+	})
+	out = target
+
+	// target value found in exclude list
+	if len(n.exclude) > i && n.exclude[i] == target {
+	loop:
+		// target value found in exclude list
+		if len(n.exclude) > i && i >= 0 {
+			out = out - n.offset
+			// check the output with in the range
+			if out <= n.begin {
+				return target
+			}
+			if i > 0 {
+				// check output fell into exclude list
+				if n.exclude[i-1] == out {
+					i = i - 1
+					goto loop
+				}
+			}
+		}
+	}
+	return out
+}
+
+func (n *NumMatch) RoundUp(target int64) (out int64) {
+	// target is not above the start
+	if target <= n.begin {
+		target = n.begin
+	}
+	// serch the traget value in exclude list
+	i := sort.Search(len(n.exclude), func(i int) bool {
+		return n.exclude[i] >= target
+	})
+	out = target
+
+	// target value found in exclude list
+	if len(n.exclude) > i && n.exclude[i] == target {
+	loop:
+		if len(n.exclude) > i && i >= 0 {
+			out = out + n.offset
+			// check the output with in the range
+			if out <= n.begin {
+				return n.begin
+			}
+			if len(n.exclude) > i {
+				// check output fell into exclude list
+				if n.exclude[i+1] == out {
+					i = i + 1
+					goto loop
+				}
+			}
+		}
+	}
+
+	return out
+}
